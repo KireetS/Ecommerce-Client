2b578036e4d80b538e922f1fd75c4fd5b0eb0698
Kireet <singhkireet5@gmail.com>
2023-10-20 10:55:36 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import Login from "./Components/Login";
+import Navbar from "./Components/Navbar";
 import Signup from "./Components/Signup";
 import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
       <div className="flex flex-col  min-h-screen min-w-screen ">
-        <div className="h-14 bg-slate-600"></div>
+        <div className="h-14">
+          <Navbar />
+        </div>
 
         <div className="h-screen flex">
           <Routes>
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="w-full h-full bg-slate-950 flex items-center justify-between">
+        <div className="flex text-center justify-center ">
+          <h1 className="text-white font-bold text-3xl">Ecommerce</h1>
+        </div>
+        <div className="flex items-center justify-center">
+          <div className="text-gray-200 font-semibold text-base mr-2 p-2">
+            Home
+          </div>
+          <div className="text-gray-200 font-semibold text-base mx-2 p-2">
+            Products
+          </div>
+          <div className="text-gray-200 font-semibold text-base ml-2 p-2">
+            Add Products
+          </div>
+        </div>
+        <div className="text-white font-semibold text-base p-4 ">User</div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
